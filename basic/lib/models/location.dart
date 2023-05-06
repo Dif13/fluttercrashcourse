8ae2de06451febd30459f8a7878812ac14f3e9b0
Рузаль Гимазов <r.gimazov@decosystems.ru>
2023-05-06 23:29:22 +0300

    Error in location.dart
    Expect at method fetchAll:
    The body might complete normally, causing 'null' to be
    returned, but the return type, 'FutureOr<List<Location>>', is a
    potentially non-nullable type.
    Try adding either a return or a throw statement at the end.

--- basic/lib/models/location.dart
+++ basic/lib/models/location.dart
@@ -1,3 +1,4 @@
+import 'package:lesson01/endpoint.dart';
 import 'location_fact.dart';
 import 'package:json_annotation/json_annotation.dart';
 
@@ -5,12 +6,17 @@ part 'location.g.dart';
 
 @JsonSerializable()
 class Location {
+  final int? id;
   final String? name;
   final String? url;
   final List<LocationFact>? facts;
 
-  Location({this.name, this.url, this.facts});
+  Location({this.id, this.name, this.url, this.facts});
 
   factory Location.fromJson(Map<String, dynamic> json) =>
       _$LocationFromJson(json);
+
+  static Future<List<Location>> fetchAll() async {
+    var uri = Endpoint.uri('/locations');
+  }
 }
